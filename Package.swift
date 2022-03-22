20e73694536a67d1f2801e3e87dbcaffd18d52ed
Serdar Coşkun <coskun.serdar@gmail.com>
2022-03-22 19:50:28 +0300

    Update Package.swift
    update dependencies for own

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [.library(name: "SoundpipeAudioKit", targets: ["SoundpipeAudioKit"])],
     dependencies: [
         .package(url: "https://github.com/AudioKit/KissFFT", from: "1.0.0"),
-        .package(url: "https://github.com/AudioKit/AudioKit", from: "5.3.0"),
-        .package(url: "https://github.com/AudioKit/AudioKitEX", from: "5.3.0"),
+        .package(url: "https://github.com/overtune-app/AudioKit", .revision("77243f19117400dd4927b5ff1ee3b21a1d5ed11f"))
+        .package(url: "https://github.com/overtune-app/AudioKitex", .revision("66b9203e32842d1af84d891805aaf30e205f9ad0")),
     ],
     targets: [
         .target(name: "Soundpipe",
