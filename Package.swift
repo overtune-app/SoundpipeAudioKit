ce5953b42e7bb75448eab3053c3db59df744e2bd
Serdar Coşkun <coskun.serdar@gmail.com>
2022-03-22 20:56:54 +0300

    Update Package.swift
    fix url case

--- Package.swift
+++ Package.swift
@@ -10,14 +10,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/AudioKit/KissFFT", from: "1.0.0"),
         .package(url: "https://github.com/overtune-app/AudioKit", .revision("77243f19117400dd4927b5ff1ee3b21a1d5ed11f")),
-        .package(url: "https://github.com/overtune-app/AudioKitex", .revision("66b9203e32842d1af84d891805aaf30e205f9ad0")),
+        .package(url: "https://github.com/overtune-app/AudioKitEX", .revision("66b9203e32842d1af84d891805aaf30e205f9ad0")),
     ],
     targets: [
         .target(name: "Soundpipe",
-                dependencies: [
-                    .product(name: "Vapor", package: "vapor"),
-                    "KissFFT"
-                ],
+                dependencies: ["KissFFT"],
                 exclude: ["lib/inih/LICENSE.txt"],
                 cSettings: [
                     .headerSearchPath("lib/inih"),
